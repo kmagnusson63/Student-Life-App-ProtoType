6307915d122f53d55d49457183561a0fc8f22a88
Lordyuri <abradford@academic.rrc.ca>
2014-02-04 11:09:04 -0600

    update from master

--- jobscentral.js
+++ jobscentral.js
@@ -1,51 +1,53 @@
 var jobList = new Array();
 
+
 function getJobList()
 {
-var jobHTTP = new XMLHttpRequest()
+    var jobHTTP = new XMLHttpRequest()
 
-jobHTTP.onreadystatechange = function(){
-   if( jobHTTP.readyState == 4 && jobHTTP.status == 200)
-   {
-      jobList = eval(jobHTTP.responseText);
-      
-      var job_table = document.getElementById("jobs_list");
-      
-      for (var i=0; i<jobList.length; i++)
-      {
-	   var ndiv = document.createElement("div");
-	   
-           var npar = document.createElement("h3");
-	   npar.textContent = jobList[i].title;
-	   //job_title_td.setAttribute("onClick", "toggle("+jobList[i].id+")")
-	   var nspan = document.createElement("span");
-	   nspan.textContent = jobList[i].closeDate;
-	   
-	   var id = jobList[i].id;
-	   nspan.setAttribute("class", "right")
-	   npar.appendChild(nspan);
-	   ndiv.appendChild(npar);
-	   ndiv.setAttribute("class", "job");
-	   ndiv.setAttribute("key", "jobList[i].id");
-	   //ndiv.addEventListener("click",function(e){ toggle(e.target.parentNode.nextSibling);}, false);
-	   
-	   var my_div = document.getElementById("job_list");
-           my_div.appendChild(ndiv);
-           
-	   // save job display elements to variable to add to table
-	   var job_display = displayJob(jobList[i]);
-	   //job_tr.addEventListener("click",toggle(jobList[i].id),false);
-      
-	}
+    jobHTTP.onreadystatechange = function(){
+        if( jobHTTP.readyState == 4 && jobHTTP.status == 200)
+        {
+            jobList = eval(jobHTTP.responseText);
+
+            var job_table = document.getElementById("job_list");
+
+            for (var i=0; i<jobList.length; i++)
+            {
+                var ndiv = document.createElement("div");
+
+                var npar = document.createElement("h3");
+                npar.textContent = jobList[i].title;
+                //job_title_td.setAttribute("onClick", "toggle("+jobList[i].id+")")
+                var nspan = document.createElement("span");
+                nspan.textContent = jobList[i].closeDate;
+
+                var id = jobList[i].id;
+                nspan.setAttribute("class", "right")
+                npar.appendChild(nspan);
+                ndiv.appendChild(npar);
+                ndiv.setAttribute("class", "job");
+                ndiv.setAttribute("key", "jobList[i].id");
+                ndiv.setAttribute("key", jobList[i].id);
+                //ndiv.addEventListener("click",function(e){ toggle(e.target.parentNode.nextSibling);}, false);
+
+                var my_div = document.getElementById("job_list");
+                job_table.appendChild(ndiv);
+
+                // save job display elements to variable to add to table
+                var job_display = displayJob(jobList[i]);
+                //job_tr.addEventListener("click",toggle(jobList[i].id),false);
+
+            }
+        }
     }
+
+    jobHTTP.open("GET","job_central.json", true);
+    jobHTTP.send();
 }
 
-jobHTTP.open("GET","job_central.json", true);
-jobHTTP.send();
-}
-
-function displayJob(jobs)
-{
+function displayJob(jobs){
+	
     var ndiv = document.createElement("div");
     var nh1 = document.createElement("h1");
     var nh3 = document.createElement("h3");
@@ -59,8 +61,7 @@ function displayJob(jobs)
     var tn1=document.createTextNode("Available positions: " + jobs.avaPos);
     var tn2=document.createTextNode(text0);
     var tn3=document.createTextNode(text1);
-    var tn4=document.createTextNode(text2);
-    
+    var tn4=document.createTextNode(text2);    
     
     nh1.appendChild(tn0);
     nh3.appendChild(tn1);
@@ -75,18 +76,15 @@ function displayJob(jobs)
     ndiv.appendChild(np2);
     
     ndiv.setAttribute("id", jobs.id);
-    //ndiv.setAttribute("class", "panel right");
-    //ndiv.setAttribute("data-role", "panel");
-    //ndiv.setAttribute("data-position", "right");
-    //ndiv.setAttribute("data-display", "overlay");
+    ndiv.setAttribute("class", "panel");
     ndiv.style.display="none";
     
-    var my_div = document.getElementById("job_list");
+    var my_div = document.getElementById("panels");
     my_div.appendChild(ndiv);
 }
 
 function toggle(id){
-	var x = id;
+	var x = document.getElementById(id);
        if(x.style.display == 'block'){
           x.style.display = 'none';
        }
@@ -95,9 +93,20 @@ function toggle(id){
        }
 }
 
-function load()
+function jobcentral_load()
 {
-	getJobList();
+    $("h3").live("swipe", function(e){
+        var key = e.target.parentNode.getAttribute("key");
+        var panelCon = document.getElementsByClassName("panel")
+
+        for(var i=0; i<panelCon.length; i++){
+            panelCon[i].style.display="none";
+        }
+
+        toggle(key);
+        $("#panels").panel("open");
+        $(".ui-panel-animate.ui-panel-open.ui-panel-position-right.ui-panel-display-overlay").css("width","100%");
+    });
+    getJobList();
 }
 
-document.addEventListener("DOMContentLoaded",load,false);
--- settings.js
+++ settings.js
@@ -1,6 +1,30 @@
+function removeStorage()
+{
+	localStorage.removeItem("device_id");
+	localStorage.removeItem("screen_name");
+}
+
+function startPage()
+{
+	document.getElementById("device_id_td").textContent = window.device_id;
+	document.getElementById("screen_name_td").textContent = window.screen_name;
+}
+
 function updateScreen()
 {
-	document.getElementById("device_name").textContent = window.device_name;
-	document.getElementById("screen_name").textContent = window.screen_name;
+	document.getElementById("device_id_td").textContent = window.device_id;
+	document.getElementById("screen_name_td").textContent = window.screen_name;
+//	document.getElementsByTagName("body")[0].style = "height:" + window.innerHeight + " !important";
 }
-document.addEventListener("DONContentLoaded",updateScreen, false);
+
+function getSettingsFromStorage()
+{
+	if(!(localStorage.getItem("device_id")))
+	{
+		localStorage.setItem("device_id","1234567890");
+		localStorage.setItem("screen_name","Bob");
+	}
+	window.device_id = localStorage.getItem("device_id");
+	window.screen_name = localStorage.getItem("screen_name");
+	updateScreen();
+}
