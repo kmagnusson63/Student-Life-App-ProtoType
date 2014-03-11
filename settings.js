8e315c2d9b77d7f725fa6568cf421bff3552bd81
Lordyuri <abradford@academic.rrc.ca>
2014-03-11 13:49:37 -0500

    commit
    commit to commit

--- settings.js
+++ settings.js
@@ -2,18 +2,24 @@ function removeStorage()
 {
 	localStorage.removeItem("device_id");
 	localStorage.removeItem("screen_name");
+    //localStorage.removeItem("password");
+    //localStorage.removeItem("avatar");
 }
 
 function startPage()
 {
 	document.getElementById("device_id_td").textContent = window.device_id;
 	document.getElementById("screen_name_td").textContent = window.screen_name;
+    //document.getElementById("password_td").textContent = window.password;
+    //document.getElementById("avatar_td").textContent = window.avatar;
 }
 
 function updateScreen()
 {
 	document.getElementById("device_id_td").textContent = window.device_id;
 	document.getElementById("screen_name_td").textContent = window.screen_name;
+    //document.getElementById("password_td").textContent = window.password;
+    //document.getElementById("avatar_td").textContent = window.avatar;
 //	document.getElementsByTagName("body")[0].style = "height:" + window.innerHeight + " !important";
 }
 
@@ -23,8 +29,12 @@ function getSettingsFromStorage()
 	{
 		localStorage.setItem("device_id","1234567890");
 		localStorage.setItem("screen_name","Bob");
+        //localStorage.setItem("password","Heatwave1");
+        //localStorage.setItem("avatar","pic.jpg");
 	}
 	window.device_id = localStorage.getItem("device_id");
 	window.screen_name = localStorage.getItem("screen_name");
+    //window.screen_name = localStorage.getItem("password");
+    //window.screen_name = localStorage.getItem("avatar");
 	updateScreen();
 }
